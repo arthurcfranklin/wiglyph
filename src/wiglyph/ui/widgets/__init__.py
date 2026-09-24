8d83e9c9cd5878f37710710cce6c7b2188b73345
arthurcfranklin <arthurdcaf@gmail.com>
2026-09-24 16:08:33 -0300

    refactor: remove unused info panel

--- src/wiglyph/ui/widgets/__init__.py
+++ src/wiglyph/ui/widgets/__init__.py
@@ -1,9 +1,7 @@
-from wiglyph.ui.widgets.info_panel import InfoPanel
 from wiglyph.ui.widgets.password_field import PasswordField
 from wiglyph.ui.widgets.qr_history import QRHistoryItem, QRHistoryWidget
 
 __all__ = [
-    "InfoPanel",
     "PasswordField",
     "QRHistoryItem",
     "QRHistoryWidget",
